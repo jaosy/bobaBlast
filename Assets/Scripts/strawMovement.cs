2d92baa6a905dc81c26042fdc95d95765d1f22aa
ivycontreras <ivycontreras88@gmail.com>
2020-02-29 12:31:32 -0600

    (hopefully) final design??
    tadaa

---
+++ Assets/Scripts/strawMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class strawMovement : MonoBehaviour
+{
+    public Vector3 from = new Vector3(0f, 0f, 135f);
+    public Vector3 to = new Vector3(0f, 0f, 225f);
+    public float speed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Mathf.PingPong(Time.time * speed * 1.0f, 1.0f);
+        transform.eulerAngles = Vector3.Lerp(from, to, t);
+    }
+}
