dd40bf0a2bbe8cf1a071b54c51880921ef046b30
Clermont028 <bclermon@macalester.edu>
2020-03-07 14:26:41 -0600

    Added a second scene
    Added a scene for home page (currently empty) this is also just a test

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class rotator : MonoBehaviour
 {
+    Transform b;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        b.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
