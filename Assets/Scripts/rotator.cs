3e759dc5c6b665d1f03323f8fda7c09410d7fd9b
ivycontreras <ivycontreras88@gmail.com>
2020-03-08 11:10:20 -0500

    master copy

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class rotator : MonoBehaviour
 {
 
+    float x;
+    float y;
+    float z;
+    Vector3 pos;
+
+    private void Start()
+    {
+        x = Random.Range(-3, 4);
+        y = 5;
+        z = Random.Range(6, 6);
+        pos = new Vector3(x, y, z);
+        transform.position = pos;
+    }
+
+
     // Update is called once per frame
     void Update()
     {
