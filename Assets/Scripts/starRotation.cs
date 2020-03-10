208e333673e6745af71849021d38276d83822e47
jaosy <jong@macalester.edu>
2020-03-09 20:16:27 -0500

    Hopeful fixes for broken star rotation
    Deleted rotator.cs script! The star still doesn't spawn inside the bubble and the straw fires backwards.

---
+++ Assets/Scripts/starRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starRotation : MonoBehaviour
+{
+    //Transform b;
+
+    float x;
+    float y;
+    float z;
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      x = Random.Range(-3,4);
+      y = 5;
+      z = Random.Range(6,6);
+      pos = new Vector3(x,y,z);
+      transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
