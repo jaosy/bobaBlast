b3eaa73f8e34bd5b5278d20d7f58fa35946dc18d
jaosy <jong@macalester.edu>
2020-03-11 20:04:26 -0500

    Boba shoots (improvements can be made)
    The boba shoots and collides with the bubble wrap sheet, but only the middle row of bubbles, and has poor horizontal range. Fixed the straw rotation script to widen the horizontal range for the boba. Definitely need to discuss changing the range or target.

--- Assets/Scripts/FireBoba.cs
+++ Assets/Scripts/FireBoba.cs
@@ -4,30 +4,57 @@ using UnityEngine;
 
 public class FireBoba : MonoBehaviour
 {
-    public GameObject bobaPrefab;
-    public float force;
+    [SerializeField]
+    public Rigidbody boba;
+    private float angle = 45f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /* Start is called before the first frame update 
+     */
+    void Start() {}
 
-    // Update is called once per frame
+    /* Update is called once per frame
+     */
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
           
-            Fire();
+            // transform.eulerAngles gives the straw's angle of rotation
+            Ray ray = new Ray(transform.position, transform.eulerAngles);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                shootBoba(hitInfo.point);
+            }
+
         }
     }
 
-    public void Fire() {
-        var boba = (GameObject)Instantiate(bobaPrefab, transform.position, transform.rotation);
-        
-        // Debug.Log("Firing!");
-        // boba.transform.Translate(transform.forward * Time.deltaTime);
-        // Debug.Log(transform.forward);
+    /* Sets boba velocity and moves the boba
+     */
+    private void shootBoba(Vector3 point)
+    {
+        var velocity = BallisticVelocity(point, angle);
+        Debug.Log("Firing at " + point + " velocity " + velocity); // console debugging
+
+        boba.transform.position = transform.position;
+        boba.velocity = velocity;
+    }
+
+    /* Calculates the velocity of the boba
+     */
+    private Vector3 BallisticVelocity(Vector3 destination, float angle)
+    {
+        Vector3 dir = destination - transform.position; // get Target Direction
+        float height = dir.y; // get height difference
+        dir.y = 0; // retain only the horizontal difference
+        float dist = dir.magnitude; // get horizontal direction
+        float a = angle * Mathf.Deg2Rad; // Convert angle to radians
+        dir.y = dist * Mathf.Tan(a); // set dir to the elevation angle.
+        dist += height / Mathf.Tan(a); // Correction for small height differences
+       
+        float velocity = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));  // Calculate the velocity magnitude
+        return velocity * dir.normalized; // Return a normalized vector.
     }
 
 }
--- Assets/Scripts/strawMovement.cs
+++ Assets/Scripts/strawMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class strawMovement : MonoBehaviour
 {
-    public Vector3 from = new Vector3(0f, 0f, 135f);
-    public Vector3 to = new Vector3(0f, 0f, 225f);
+    public Vector3 from = new Vector3(0f, 0f, 100f);
+    public Vector3 to = new Vector3(0f, 0f, 255f);
     public float speed = 1.0f;
 
     // Update is called once per frame
