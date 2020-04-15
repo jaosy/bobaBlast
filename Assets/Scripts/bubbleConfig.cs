709fdcc04d509dd56a3bc999e24b1cb82b88ff01
Clermont028 <Bclermon@macalester.edu>
2020-04-15 14:51:43 -0400

    Refactored Bubble Set Up script
    Made the code easier to read

--- Assets/Scripts/bubbleConfig.cs
+++ Assets/Scripts/bubbleConfig.cs
@@ -15,39 +15,33 @@ public class bubbleConfig : MonoBehaviour
     {
         //Starting Values: 
         Vector3 wrapPos = GetComponent<Transform>().position;
-        Vector3 startPos = new Vector3(-3.83f, 7.28f, 6.33f);
+        Vector3 rowStart = new Vector3(-3.83f, 7.28f, 6.33f);
 
         //Moving Vectors
-        Vector3 angle = Vector3.Cross(startPos, wrapPos).normalized; // Gives the right depth for the bubble on the sheet
-        Vector3 across = new Vector3(1.5f, 0, 0);
-        Vector3 zeroed = new Vector3(0, 0, 1);
-        Vector3 down = new Vector3(0, -1.5f, 0);
+        Vector3 angle = Vector3.Cross(rowStart, wrapPos).normalized; // Gives the right depth for the bubble on the sheet
+        Vector3 stepAcross = new Vector3(1.5f, 0, 0);
+        Vector3 zeroed = new Vector3(0, 0, 1); 
+        Vector3 stepDown = Vector3.Scale(zeroed, angle) + new Vector3(0, -1.5f, 0);
 
-        //Steps
-        Vector3 oldPos; //Keep track of last places bubble
-        Vector3 newPos; //Where to place the next bubble
+        //Steps 
+        Vector3 currPos; //Where to place the next bubble
 
-        Debug.Log("Wrap Position: " + wrapPos + "\tStart Position: " + startPos);
+        Debug.Log("Wrap Position: " + wrapPos + "\tStart Position: " + rowStart);
 
-        for (int r = 0; r <= 3; ++r)
+        for (int row = 0; row <= 3; ++row)
         {
-            oldPos = startPos;  //Resets 
-            newPos = oldPos;
-            for (int i = 1; i <= 6; ++i)
+            currPos = rowStart;
+            for (int col = 1; col <= 6; ++col)
             {
 
-                GameObject instBubble = Instantiate(bubble, newPos, Quaternion.identity);
+                GameObject instBubble = Instantiate(bubble, currPos, Quaternion.identity);
                 instBubble.GetComponent<bubble>().addStar();
 
-                newPos = oldPos + across;
+                currPos += stepAcross;
 
-                Debug.Log("Moved from: " + oldPos + "to \tNew Position " + newPos
-                          + "\nBy" + across + "Across and: " + down + "Down");
-
-                oldPos = newPos;
             }
 
-            startPos = startPos + (Vector3.Scale(zeroed, angle)) + down;
+            rowStart += stepDown;
         }
 
     }
