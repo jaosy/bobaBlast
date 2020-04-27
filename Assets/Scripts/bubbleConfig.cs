d9ff1716ff88230d6d4bfeb306fdbaa0a651302d
ivycontreras <ivycontreras88@gmail.com>
2020-04-27 15:53:02 -0700

    Angle of Bubbles

--- Assets/Scripts/bubbleConfig.cs
+++ Assets/Scripts/bubbleConfig.cs
@@ -15,19 +15,17 @@ public class bubbleConfig : MonoBehaviour
     void Start()
     {
         //Starting Values: 
-        Vector3 wrapPos = GetComponent<Transform>().position;
+        Transform sheetTransform = GetComponent<Transform>();
         Vector3 rowStart = new Vector3(-3.83f, 7.28f, 6.33f);
 
         //Moving Vectors
-        Vector3 angle = Vector3.Cross(rowStart, wrapPos).normalized; // Gives the right depth for the bubble on the sheet
-        Vector3 stepAcross = new Vector3(bubbleSpacing, 0, 0);
-        Vector3 zeroed = new Vector3(0, 0, 1); 
-        Vector3 stepDown = (Vector3.Scale(zeroed, angle) + new Vector3(0, -bubbleSpacing, 0)) * Mathf.Sqrt(0.75f);
+        Vector3 stepAcross = sheetTransform.right * bubbleSpacing; 
+        Vector3 stepDown = sheetTransform.forward * -bubbleSpacing * Mathf.Sqrt(0.75f);
 
         //Steps 
         Vector3 currPos; //Where to place the next bubble
 
-        Debug.Log("Wrap Position: " + wrapPos + "\tStart Position: " + rowStart);
+       
 
         for (int row = 0; row <= 5; ++row)
         {
